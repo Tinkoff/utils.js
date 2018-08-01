99a42180a295a443c6ff45e8a591e287a64783c0
m.khasanov <m.khasanov@tinkoff.ru>
2018-08-01 18:47:32 +0300

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-    testMatch: ['**/__tests__/**/*.js?(x)'],
-    timers: 'fake'
+    testMatch: [
+        '**/__tests__/**/*.js?(x)'
+    ],
+    timers: 'fake',
+    testURL: 'http://localhost/'
 };
