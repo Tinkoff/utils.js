2885034915b098bc773d2f0ed1464f15bb685f41
osorokotyaga <35488154+osorokotyaga@users.noreply.github.com>
2018-09-18 16:22:27 +0300

    Remove sanitize util. (#12)
    src/sanitize was too dumb, better use specially oriented sanitize packages
    
    Resolves: #3

--- src/__tests__/sanitize.js
+++
@@ -1,9 +0,0 @@
-import sanitize from '../sanitize';
-
-describe('utils/sanitize', () => {
-    it('test', () => {
-        expect(sanitize('em', 'test123')).toBe('test123');
-        expect(sanitize('em', '<em>abc</em>')).toBe('abc');
-        expect(sanitize('unknown', '<em>123</em>')).toBe('<em>123</em>');
-    });
-});
