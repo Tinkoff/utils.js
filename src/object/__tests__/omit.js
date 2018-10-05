48d83a9938ae4db10eaae285ae6398c0b0c02002
osorokotyaga <o.sorokotyaga@andersensoft.ru>
2018-10-05 18:04:15 +0300

    object/omit add test for string conversion bug

--- src/object/__tests__/omit.js
+++ src/object/__tests__/omit.js
@@ -6,4 +6,9 @@ describe('utils/object/omit', () => {
         expect(omit(['a', 'c', 'd'], { a: 2 })).toEqual({});
         expect(omit([], { a: 3 })).toEqual({ a: 3 });
     });
+
+    it('omit should convert keys to string', () => {
+        expect(omit([1, 'a'], { 1: 'a' })).toEqual({});
+        expect(omit([2, 'a'], { 2: 2, a: 'a', b: 'b' })).toEqual({ b: 'b' });
+    });
 });
