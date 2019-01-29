1feb5503c3c28ee942fcdfcbb2c84d5e8c683571
osorokotyaga <o.sorokotyaga@andersensoft.ru>
2019-01-29 13:25:40 +0300

    Fix array/maxBy edge cases
    Resolves: #29

--- src/array/__tests__/maxBy.js
+++ src/array/__tests__/maxBy.js
@@ -6,5 +6,17 @@ describe('utils/array/maxBy', () => {
 
         expect(maxBy(f, [1, 2, 3, 4, 5])).toBe(1);
         expect(f).toHaveBeenCalledTimes(5);
+
+        expect(maxBy(f, [5, 2, 3])).toBe(2);
+    });
+
+    it('test edge cases', () => {
+        const f = jest.fn(x => x);
+
+        expect(maxBy(f, [])).toBeUndefined();
+        expect(f).not.toHaveBeenCalled();
+
+        expect(maxBy(f, [1])).toBe(1);
+        expect(f).toHaveBeenCalledTimes(1);
     });
 });
