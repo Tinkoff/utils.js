f6f8d7468d266fdad59129442a321f76520f26e4
osorokotyaga <o.sorokotyaga@andersensoft.ru>
2019-01-31 11:32:09 +0300

    fix docs generation
    enable docs generation for typescript
    fix missing files on gh-pages
    
    Resolves: #33

--- generate/docs/index.js
+++ generate/docs/index.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const fsExtra = require('fs-extra');
+const execa = require('execa');
 const ESDocCLI = require('esdoc/out/src/ESDocCLI').default;
 const ghpages = require('gh-pages');
 
 const root = path.join(__dirname, '..', '..');
 const docsPath = path.join(root, 'docs');
+const tsTemp = path.join(root, 'ts_temp');
 
-const clearDocsPath = () => fsExtra.remove(docsPath);
+const clearDocsPath = () =>
+    Promise.all([fsExtra.remove(docsPath), fsExtra.remove(tsTemp)]);
 
 const runEsdoc = () => {
     const cli = new ESDocCLI([]);
@@ -14,16 +17,32 @@ const runEsdoc = () => {
     cli.exec();
 };
 
-const copyErrorPage = () => fsExtra.copy(path.join(__dirname, '404.html'), path.join(docsPath, '404.html'));
+const copyFiles = () =>
+    Promise.all([
+        fsExtra.copy(
+            path.join(__dirname, '404.html'),
+            path.join(docsPath, '404.html')
+        ),
+        fsExtra.copy(
+            path.join(__dirname, '.nojekyll'),
+            path.join(docsPath, '.nojekyll')
+        ),
+    ]);
 
-const publishDocs = () => new Promise((resolve, reject) => {
-    ghpages.publish(docsPath, err => (err ? reject(err) : resolve(err)));
-});
+const publishDocs = () =>
+    new Promise((resolve, reject) => {
+        ghpages.publish(docsPath, { dotfiles: true }, (err) =>
+            err ? reject(err) : resolve(err)
+        );
+    });
+
+const buildTs = () => execa.shell('tsc -p tsconfig.json');
 
 const generate = async () => {
     await clearDocsPath();
+    await buildTs();
     await runEsdoc();
-    await copyErrorPage();
+    await copyFiles();
     await publishDocs();
     await clearDocsPath();
 };
