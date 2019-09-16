c60bf599d48385ef31edd6438241c071c11192ae
a.marchenko <a.marchenko@tinkoff.ru>
2019-09-16 17:11:22 +0300

    Update test libs to last version

--- src/is/__tests__/reactElement.ts
+++ src/is/__tests__/reactElement.ts
@@ -1,19 +1,19 @@
-const { createElement, PureComponent } = require('react');
-
-let mockReactIs;
-
-jest.mock('react-is', () => mockReactIs);
-
-class Component extends PureComponent {}
-
 describe('utils/is/reactElement', () => {
+    const { createElement, PureComponent } = require('react');
+
+    let mockReactIs;
+
+    jest.mock('react-is', () => mockReactIs);
+
+    class Component extends PureComponent { }
+
     beforeEach(() => {
         jest.resetModules();
     });
 
     it('test', () => {
         mockReactIs = require.requireActual('react-is');
-        const isReactElement = require('../reactElement');
+        const isReactElement = require('../reactElement').default;
 
         expect(isReactElement('test')).toBe(false);
         expect(isReactElement({})).toBe(false);
@@ -24,7 +24,7 @@ describe('utils/is/reactElement', () => {
 
     it('test when is-react not defined', () => {
         mockReactIs = null;
-        const isReactElement = require('../reactElement');
+        const isReactElement = require('../reactElement').default;
 
         expect(isReactElement('test')).toBe(false);
         expect(isReactElement({})).toBe(false);
