777b7412ab680042349c316a88d33bcd681c470f
Alexey Ryazanov <al.ryazanov@tinkoff.ru>
2023-07-11 15:33:00 +0500

    feat(ci): add release workflow with `release-it`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# 2.2.1
+## 2.2.1
 
 + add check into the utility isEqual for comparision of functions by reference (#63)
 
--- releaseUtils/publish.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-commonjs, global-require */
-const execa = require('execa');
-
-const publish = (dir, npmTag) =>
-    execa.shell(`npm publish  --access public ${dir} --tag ${npmTag}`);
-
-module.exports = publish;
