49c0b80a35085a143f7d017b15b7657f5b0fbfd6
Alexey Ryazanov <al.ryazanov@tinkoff.ru>
2023-07-31 21:31:10 +0500

    fix: add files to publishing

---
+++ tools/copy-release-files.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fsExtra = require('fs-extra');
+const recursiveReadSync = require('recursive-readdir-sync');
+
+const ROOT_PATH = path.join(__dirname, '..')
+const BUILD_PATH = path.join(ROOT_PATH, 'build')
+
+const copyReleaseFiles = async () => {
+    await Promise.all(
+        recursiveReadSync(path.join(ROOT_PATH, 'ts_temp'))
+            .filter(fileName => fileName.endsWith('.d.ts'))
+            .map(fileName =>
+                fsExtra.copy(
+                    fileName,
+                    fileName.replace('ts_temp', 'build')
+                )
+            )
+    );
+
+    await Promise.all(
+        ['package.json', '.npmignore', 'README.md', 'LICENSE', 'CHANGELOG.md'].map(fileName =>
+            fsExtra.copy(
+                path.join(ROOT_PATH, fileName),
+                path.join(BUILD_PATH, fileName)
+            )
+        )
+    );
+}
+
+copyReleaseFiles().then(() => {})
